39e058aa8f7f4d47ea9f491e9afb7b4cd1e27d0a
angeloDesear <angelodesear2002@gmail.com>
2024-08-29 14:09:08 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Add any JavaScript functionality here
+
+document.getElementById('loginBtn').addEventListener('click', function() {
+    alert('Login functionality not implemented yet.');
+});
+
+document.getElementById('joinBtn').addEventListener('click', function() {
+    alert('Join us functionality not implemented yet.');
+});
